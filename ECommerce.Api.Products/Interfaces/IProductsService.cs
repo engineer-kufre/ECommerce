282dced26da2e52212225184bb75261e95b9479f
Kufreabasi <engineer_kufre@yahoo.com>
2024-01-05 10:49:13 +0100

    Add project files.

---
+++ ECommerce.Api.Products/Interfaces/IProductsService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Api.Products.DTOs;
+
+namespace ECommerce.Api.Products.Interfaces
+{
+    public interface IProductsService
+    {
+        Task<(bool IsSuccess, IEnumerable<ProductDto> Products, string ErrorMessage)> GetProductsAsync();
+    }
+}
